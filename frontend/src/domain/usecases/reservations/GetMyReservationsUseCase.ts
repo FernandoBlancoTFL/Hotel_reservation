95fd3800f91c0107b9b0833645798dd642cbddd8
Fernando Blanco <71463571+FernandoBlancoTFL@users.noreply.github.com>
2025-11-04 14:30:18 -0300

    Delete frontend directory

--- frontend/src/domain/usecases/reservations/GetMyReservationsUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { ReservationRepository } from '../../repositories/ReservationRepository';
-
-export class GetMyReservationsUseCase {
-  constructor(private reservationRepository: ReservationRepository) {}
-
-  async execute() {
-    return await this.reservationRepository.getMyReservations();
-  }
-}
